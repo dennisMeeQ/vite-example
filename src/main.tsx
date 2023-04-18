463a5e4146903bccf1b01b0fc6be213760f5e81b
Dennis Fiedler <dennis.fiedler@ysura.com>
2023-04-18 13:41:53 +0200

    update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import * as ReactDOM from 'react-dom';
 import { App } from '@/App';
 import { ThemeProvider } from '@/themes';
 
-// We wait for the service worker to start before rendering the application
 ReactDOM.render(
   <StrictMode>
     <ThemeProvider>
